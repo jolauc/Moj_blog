aafa25c2c1e246023b3ce7ec3986e4835b4e4c0f
Jola Fasola <jolauc@poczta.onet.pl>
2017-05-27 21:35:15 +0200

    Zmieniony kod HTML na stronie.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.conf.urls import patterns, include, url
+#from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 
 admin.autodiscover()
 
 
-urlpatterns = patterns('',
-   url(r'^admin/', include(admin.site.urls)),
-   url(r'^/?$', 'news.views.index'),)
+#urlpatterns = patterns('',
+#   url(r'^admin/', include(admin.site.urls)),
+#   url(r'^/?$', 'news.views.index'),)
 
 
     # Examples:
