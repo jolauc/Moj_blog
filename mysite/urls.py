7abd14b2218d06be5b0cb375753ace1fece93dbf
Jola Fasola <jolauc@poczta.onet.pl>
2017-06-02 10:54:37 +0200

    Zmieniony kod HTML na stronie.

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,10 +6,11 @@ urlpatterns = [
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
 	url(r'', include('blog.urls')),
 	url(r'^blog/', include('blog.urls')),
 	url(r'^news/',include('news.urls')),
+	url(r'^polls/', include('polls.urls')),
 ]
 
 
--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,5 @@
-from django.shortcuts import render
-from django.template import RequestContext
-from django.shortcuts import render_to_response
-
-from polls.models import *
+from django.http import HttpResponse
 
 
 def index(request):
-    polls = Question.objects.all().order_by('-posted_date')
-    return render_to_response('index.html',
-            {'polls': polls},
-            context_instance=RequestContext(request))
-# Create your views here.
-
-# Create your views here.
+    return HttpResponse("Hello, world. You're at the polls index.")
